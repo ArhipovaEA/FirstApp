f730cd31087f9f73b959c72126e19b02de4dd928
ArhipovaEA <ArhipovaEA@arkona36.ru>
2021-02-25 21:40:45 +0300

    Задание 3.3.2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,9 +6,17 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            string MyName;
-            MyName = "Ekaterina";
-            Console.WriteLine(MyName);
+            string MyName = "Jane";
+            byte MyAge = 27;
+            bool HavePet = true;
+            double MyShoeSize = 37.5;
+
+            Console.WriteLine("My name is " + MyName);
+
+            Console.WriteLine("My age is " + MyAge);
+            Console.WriteLine("Do I have a pet? " + HavePet);
+            Console.WriteLine("My shoe size is "+ MyShoeSize);
+          
             Console.ReadKey();
         }
     }
