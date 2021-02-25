db4c41de5e842828dea1b3149e5ccf708f01eb4a
ArhipovaEA <ArhipovaEA@arkona36.ru>
2021-02-25 21:30:49 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string MyName;
+            MyName = "Ekaterina";
+            Console.WriteLine(MyName);
+            Console.ReadKey();
+        }
+    }
+}
