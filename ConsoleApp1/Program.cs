ac791bae8bc9fa35897ec0b288f5851c17c9c7f2
ArhipovaEA <ArhipovaEA@arkona36.ru>
2021-02-25 22:26:29 +0300

    изучаем модуль

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,14 +10,41 @@ namespace ConsoleApp1
             byte MyAge = 27;
             bool HavePet = true;
             double MyShoeSize = 37.5;
+            DaysOfWeek MyFavoriteDay;
 
             Console.WriteLine("My name is " + MyName);
 
             Console.WriteLine("My age is " + MyAge);
             Console.WriteLine("Do I have a pet? " + HavePet);
             Console.WriteLine("My shoe size is "+ MyShoeSize);
-          
+
+            MyFavoriteDay = DaysOfWeek.Friday;
+            Console.WriteLine(MyFavoriteDay);
+
+            byte age = (byte)int.Parse(Console.ReadLine());
+            Console.WriteLine(age);
+
+
             Console.ReadKey();
-        }
+
+         }
+    }
+
+    enum DaysOfWeek : byte
+    {
+        Monday = 1,
+        Tuesday,
+        Wednesday,
+        Thurday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+
+    enum Semaphore : int
+    {
+        Red = 100,
+        Yellow = 200,
+        Green = 300
     }
 }
